eecf1a0011b502b8d75f4eb84b1322de6c956f36
pepnochizzy <laragray@live.co.uk>
2025-12-16 15:58:54 +0000

    feat: adding post and get routes, REQUIRES form.js submit handler

--- server/server.js
+++ server/server.js
@@ -17,3 +17,28 @@ app.listen(PORT, () => {
 app.get("/", (req, res) => {
   res.json(`Welcome to the root route!`);
 });
+
+app.post("/create-prescription", (req, res) => {
+  //recieve data
+  const newPrescription = req.body.formValues;
+  console.log(newPrescription);
+  //insert data into table
+  const query = db.query(
+    `INSERT INTO prescriptions (medication_name, dosage, how_often, time_of_day, re_order) VALUES ($1, $2, $3, $4, $5)`,
+    [
+      newPrescription.medicationName,
+      newPrescription.dosage,
+      newPrescription.howOften,
+      newPrescription.timeOfDay,
+      newPrescription.reOrder,
+    ]
+  );
+  res.json({ status: "success!", values: newPrescription });
+});
+
+app.get("/get-info", async (req, res) => {
+  const query = await db.query(`SELECT * FROM prescriptions`);
+  console.log(query);
+  res.json(query.rows);
+});
+//ensure newPrescription variable carries through form.js and that form names/for are camelcase in form.html
