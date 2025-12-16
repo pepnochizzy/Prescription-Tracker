a51b92b25f019364ec1f02537baad6d7b037ebd4
pepnochizzy <laragray@live.co.uk>
2025-12-16 12:16:22 +0000

    chore: vite config changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,16 @@
-import { resolve } from "path";
+import { dir } from "console";
+import { dirname, resolve } from "path";
 import { defineConfig } from "vite";
+
+const __dirname = dirname("client");
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        form: resolve(__dirname, "form/index.html"),
-        tracker: resolve(__dirname, "tracker/index.html"),
+        form: resolve(__dirname, "src/form/index.html"),
+        tracker: resolve(__dirname, "src/tracker/index.html"),
       },
     },
   },
