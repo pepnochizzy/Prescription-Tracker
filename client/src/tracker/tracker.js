892e9822e4ac61bf6d59d111aed12996d8ba5f9f
Dylan <dylan.smith3107@gmail.com>
2025-12-17 14:36:39 +0000

    feat: overview section loop has been added, basic styling a hide/show toggle also working

--- client/src/tracker/tracker.js
+++ client/src/tracker/tracker.js
@@ -5,6 +5,7 @@ async function getMedicationData() {
   const medicineData = await response.json();
   console.log(medicineData);
   renderLeftToTake(medicineData);
+  renderOverview(medicineData);
 }
 getMedicationData();
 
@@ -27,4 +28,44 @@ async function renderLeftToTake(medicineData) {
     );
     notTakenMedDiv.appendChild(medicationP);
   }
+}
+
+//Overview section
+
+async function renderOverview(medicineData) {
+  const medName = document.getElementById("medName");
+  for (let i = 0; i < medicineData.length; i++) {
+    const med = document.createElement("p");
+    med.textContent = `> ${medicineData[i].medication_name}`;
+    medName.appendChild(med);
+
+    const medInfo = document.createElement("ul");
+    medInfo.classList = "hide";
+    medName.appendChild(medInfo);
+
+    const dosage = document.createElement("li");
+    dosage.textContent = `Dosage: ${medicineData[i].dosage}`;
+    medInfo.appendChild(dosage);
+
+    const howOften = document.createElement("li");
+    howOften.textContent = `Take ${medicineData[i].how_often}`;
+    medInfo.appendChild(howOften);
+
+    const timeOfDay = document.createElement("li");
+    timeOfDay.textContent = `Take at ${medicineData[i].time_of_day}`;
+    medInfo.appendChild(timeOfDay);
+
+    const reOrder = document.createElement("li");
+    reOrder.textContent = `Re-order ${medicineData[i].re_order}`;
+    medInfo.appendChild(reOrder);
+
+    med.addEventListener("click", () => {
+      medInfoDisplay(medInfo);
+    });
+  }
+}
+
+function medInfoDisplay(medInfo) {
+  medInfo.classList.toggle(`hide`);
+  medInfo.classList.toggle(`med-info`);
 }
