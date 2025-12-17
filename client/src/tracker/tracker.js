8ced2509038af30d32ca9488a6e8dea01e855ee2
pepnochizzy <laragray@live.co.uk>
2025-12-17 16:22:46 +0000

    feat: formatting done for tracker page and nav bar formatting for tracker and form page

--- client/src/tracker/tracker.js
+++ client/src/tracker/tracker.js
@@ -18,18 +18,22 @@ function createPtag(text, i) {
 
 //dom manipulation to add medication names into section 1 and 2
 async function renderLeftToTake(medicineData) {
+  //get divs from html
   const notTakenMedDiv = document.getElementById("notTakenmedName");
-  const timeToTakeDiv = document.getElementById("timeTaken");
+  const timeToTakeDiv = document.getElementById("timeToTake");
+  //create titles for top of table and append
   const nameTitle = createPtag("Medication", "title");
   const takeTitle = createPtag("Time to take", "title");
   notTakenMedDiv.append(nameTitle);
   timeToTakeDiv.append(takeTitle);
+  //loop to create p tags for name and time for each medication
   for (let i = 0; i < medicineData.length; i++) {
     const medicationP = createPtag(
       medicineData[i].medication_name,
       "medicineName"
     );
+    const timeP = createPtag(medicineData[i].time_of_day, "timeToTake");
     notTakenMedDiv.append(medicationP);
-    timeToTakeDiv.append(); //times for medication
+    timeToTakeDiv.append(timeP); //times for medication
   }
 }
